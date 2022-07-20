6356bb566c16139e032bd74955ff685e6e879e2e
filipilicpr1 <101191574+filipilicpr1@users.noreply.github.com>
2022-07-20 23:42:34 +0200

    Connection test

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -1,5 +1,18 @@
-
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    fetch("https://localhost:44386/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "CAO",
+        password: "Zdravo",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(res => res.json()
+    ).then(data => console.log(data)).catch(err => alert(err.message));
+  }, []);
   return (
     <div>
       <h1>Hello World!</h1>
